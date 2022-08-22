fbd3592588a2f50dad28451f61277a8891254025
Netherman <111619311+Netherman@users.noreply.github.com>
2022-08-22 00:12:35 -0500

    New companion Anims

--- src/main/java/com/daltoncash/mmostats/entities/client/Companion.java
+++ src/main/java/com/daltoncash/mmostats/entities/client/Companion.java
@@ -1,15 +1,25 @@
 package com.daltoncash.mmostats.entities.client;
 
+import com.daltoncash.mmostats.entities.client.companion_goals.LookAtPlayer;
+
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.BegGoal;
 import net.minecraft.world.entity.ai.goal.BreedGoal;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.FollowOwnerGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.LeapAtTargetGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
@@ -21,10 +31,11 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NonTameRandomTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
-import net.minecraft.world.entity.animal.Turtle;
 import net.minecraft.world.entity.monster.AbstractSkeleton;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.ExtendedServerListData;
 
 public class Companion extends TamableAnimal {
 
@@ -34,22 +45,27 @@ public class Companion extends TamableAnimal {
 	
 	@Override
 	protected void registerGoals() {
-		this.goalSelector.addGoal(1, new FloatGoal(this));
+	      
 	      this.goalSelector.addGoal(2, new SitWhenOrderedToGoal(this));
 	      this.goalSelector.addGoal(4, new LeapAtTargetGoal(this, 0.4F));
 	      this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
 	      this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
-	      this.goalSelector.addGoal(7, new BreedGoal(this, 1.0D));
-	      this.goalSelector.addGoal(8, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-	      this.goalSelector.addGoal(10, new LookAtPlayerGoal(this, Player.class, 8.0F));
-	      this.goalSelector.addGoal(10, new RandomLookAroundGoal(this));
-	      this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
-	      this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
-	      this.targetSelector.addGoal(3, (new HurtByTargetGoal(this)).setAlertOthers());
-	      this.targetSelector.addGoal(6, new NonTameRandomTargetGoal<>(this, Turtle.class, false, Turtle.BABY_ON_LAND_SELECTOR));
-	      this.targetSelector.addGoal(7, new NearestAttackableTargetGoal<>(this, AbstractSkeleton.class, false));
+	      this.goalSelector.addGoal(2, new LookAtPlayer(this,  Player.class, 8.0F));
+	      //this.goalSelector.addGoal(1, new HeadTiltGoal(this));
+	     
 	}
+	//animations
+	public static final AnimationDefinition MOVEHEAD = AnimationDefinition.Builder.withLength(1f).addAnimation
+			("undefined", new AnimationChannel(AnimationChannel.Targets.ROTATION, new Keyframe
+					(0f, KeyframeAnimations.degreeVec(0f, 0f, 0f), AnimationChannel.Interpolations.LINEAR), new Keyframe
+					(1f, KeyframeAnimations.degreeVec(-12.45392054358581f, 1.0808891363294606f, -15.117940229032683f), AnimationChannel.Interpolations.LINEAR))).addAnimation
+			("undefined", new AnimationChannel(AnimationChannel.Targets.ROTATION, new Keyframe(0f, KeyframeAnimations.degreeVec(0f, 0f, 0f), AnimationChannel.Interpolations.LINEAR), new Keyframe
+					(1f, KeyframeAnimations.degreeVec(-12.45392054358581f, 1.0808891363294606f, -15.117940229033138f), AnimationChannel.Interpolations.LINEAR))).addAnimation
+			("undefined", new AnimationChannel(AnimationChannel.Targets.ROTATION, new Keyframe(0f, KeyframeAnimations.degreeVec(0f, 0f, 0f), AnimationChannel.Interpolations.LINEAR), new Keyframe
+					(1f, KeyframeAnimations.degreeVec(-12.45392054358581f, 1.0808891363294606f, -15.117940229032683f), AnimationChannel.Interpolations.LINEAR))).build();
 	
+
+	//end of animations
 	public static AttributeSupplier.Builder createAttributes() {
 	      return Mob.createMobAttributes().add(Attributes.MOVEMENT_SPEED, (double)0.3F).add(Attributes.MAX_HEALTH, 30.0D).add(Attributes.ATTACK_DAMAGE, 0.5D);
 	   }
@@ -58,6 +74,6 @@ public class Companion extends TamableAnimal {
 	public AgeableMob getBreedOffspring(ServerLevel  level, AgeableMob parent) {
 		return EntityInit.COMPANION.get().create(level);
 	}
-		
+
 
 }
